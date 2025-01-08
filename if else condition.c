d897f35167f90406bbf8b2381da67b9ee3a8c99b
Shubhangimoon <shubhangimoon790@gmail.com>
2025-01-08 22:55:57 +0530

    Add files via upload

---
+++ if else condition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int Age;
+	printf("Enter  Age");
+	scanf("%d",&Age);
+	if(Age>=18){
+		printf("you can allow for voting");
+	}
+	else{
+		printf("you can't allow");
+	}
+}
+
+
+
