9845ecbec3201f2d421c0fe370d83c1d23f0556e
Shubhangimoon <shubhangimoon790@gmail.com>
2025-01-17 09:59:11 +0530

    Add files via upload

---
+++ day6/do-whileloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i=1,j;
+	do
+	{
+	   j=1;
+	   do{
+	   	printf("*");
+	   	j++;
+	   }
+	   while(j<=i);
+	   	printf("\n");
+	   	i++;
+	   }
+	   while(i<=4); 
+}
